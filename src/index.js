2e9f6e8b32d10ade6f926384fad7c6e41e12a25f
Danielle Beyer <dwishnuff@gmail.com>
2020-11-17 23:55:15 -0800

    updated index.js to render title and grid

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import Grid from './Grid';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import Grid from "./Grid";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
-  //<App />,
-  <Grid />,
-  document.getElementById('root')
+  <div>
+    <App />
+    <Grid />
+  </div>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
