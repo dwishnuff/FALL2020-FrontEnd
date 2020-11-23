8165e2f91631c5ed70d92edced7621c80c6aae03
Danielle Beyer <dwishnuff@gmail.com>
2020-11-23 01:53:08 -0800

    updated css background

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import graphic from './hands.png';
 import './App.css';
 
 function App() {
   return (
+    <div class="banner">
+    <div class="title">
     <h1>Diversity in CS at PSU</h1>
+    </div>
+    {/*<div class="graphic">
+    //<img src={graphic} alt="image of raised diverse hands"/>
+</div>*/}
+    </div>
   );
 }
 
