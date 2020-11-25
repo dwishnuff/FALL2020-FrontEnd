452b58db1f17432565ed04cc1dc8032771a6739e
Evan Johnson <evanj2357@gmail.com>
2020-11-24 18:56:55 -0800

    add unzip

--- src/apis/utils.js
+++ src/apis/utils.js
@@ -1,10 +1,16 @@
-export { range, zip };
+export { range, unzip, zip };
 
 // Range from start (inclusive) to end (exclusive)
 function range(start, end, interval=1) {
     const diff = end - start;
-    const len = Math.floor(diff / interval + (diff % 2));
-    return Array(len).keys().map(i => interval * i + start);
+    const len = Math.floor(diff / interval + !!(diff % interval));
+    return [...Array(len).keys()].map(i => interval * i + start);
+}
+
+function unzip(arr) {
+    const inner_len = Math.min(...arr.map(a => a.length));
+
+    return range(0, inner_len).map(i => arr.map(a => a[i]));
 }
 
 function zip(a, b) {
