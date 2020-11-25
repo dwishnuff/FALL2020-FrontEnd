0f0314650fcbd7878297c3dea7e88401bf6e5b05
Evan Johnson <evanj2357@gmail.com>
2020-11-24 16:07:17 -0800

    move to api utilities folder and clean up lints

--- src/apis/ipeds.js
+++ src/apis/ipeds.js
@@ -9,29 +9,31 @@ representing a different subset of that unitid's population such as
 full-time/part-time/total.
 */
 
+export { getFallRetention };
+
 // latest year for retention data
 const LATEST = 2018
 
 const BASE = "https://educationdata.urban.org/"
 const API = "api/v1/college-university/ipeds/"
 
-// TODO: make it work
+// get overall undergrad fall retention rates for years from start to end, inclusive
 async function getFallRetention(unitid, start=LATEST, end=LATEST) {
     if (!unitid) { return null; }
 
     const api = API + "fall-retention"
 
     // supported in all browsers that matter, i.e. not IE
-    params = new URLSearchParams();
+    let params = new URLSearchParams();
     params.append("unitid", unitid);
     // code for total student body data
     params.append("ftpt", 99);
 
-    res = []
-    for (year=start; year<=end; year++) {
-        url = new URL(`${api}/${year}/?${params}`, BASE);
+    let res = []
+    for (let year=start; year<=end; year++) {
+        let url = new URL(`${api}/${year}/?${params}`, BASE);
 
-        r = await fetch(url).then((e) => {
+        let r = await fetch(url).then((e) => {
             // handle HTTP response code
             if (e.ok) {
                 return e.json();
@@ -50,7 +52,3 @@ async function getFallRetention(unitid, start=LATEST, end=LATEST) {
 
     return res;
 }
-
-
-// For testing purposes only:
-console.log(await getFallRetention(209551, 2016, 2018))
