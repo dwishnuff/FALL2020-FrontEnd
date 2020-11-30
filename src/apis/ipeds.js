bbb63c6ecb9a068e4ef7c19938c25eb00636f87a
Evan Johnson <evanj2357@gmail.com>
2020-11-29 16:28:31 -0800

    adjust indentation for consistency

--- src/apis/ipeds.js
+++ src/apis/ipeds.js
@@ -9,84 +9,109 @@ representing a different subset of that unitid's population such as
 full-time/part-time/total.
 */
 
-import * as _ from './ipeds_consts.js';
-import { unzip } from './utils.js';
+import * as _ from "./ipeds_consts.js";
+import { unzip } from "./utils.js";
 
 export { getFallRetention, getFallEnrollmentByRace };
-export * from './ipeds_consts.js';
+export * from "./ipeds_consts.js";
 
 const BASE = "https://educationdata.urban.org/";
 const API = "api/v1/college-university/ipeds/";
 
 // should return an array of { label: int_code, data: [ints] } where the int_code is an
 // integer code corresponding to a race listed in the IPEDS data
-async function getFallEnrollmentByRace(unitid, start=_.EARLIEST, end=_.LATEST, levelOfStudy=_.ALL) {
-    if (!unitid) { return null; }
+async function getFallEnrollmentByRace(
+	unitid,
+	start = _.EARLIEST,
+	end = _.LATEST,
+	levelOfStudy = _.ALL
+) {
+	if (!unitid) {
+		return null;
+	}
 
-    const api = API + _.FALL_ENROLLMENT;
-    const endpt = "race/sex/";
+	const api = API + _.FALL_ENROLLMENT;
+	const endpt = "race/sex/";
 
-    let params = new URLSearchParams();
-    params.append("unitid", unitid);
-    params.append("sex", 99);
+	let params = new URLSearchParams();
+	params.append("unitid", unitid);
+	params.append("sex", 99);
 
-    let res = []
-    for (let year=start; year<=end; year++) {
-        let url = new URL(`${api}/${year}/${levelOfStudy}/${endpt}?${params}`, BASE);
+	let res = [];
+	for (let year = start; year <= end; year++) {
+		let url = new URL(
+			`${api}/${year}/${levelOfStudy}/${endpt}?${params}`,
+			BASE
+		);
 
-        let r = await fetch(url).then((e) => {
-            // handle HTTP response code
-            if (e.ok) {
-                return e.json();
-            }
-            // TODO: handle errors
-        }).then((data) => {
-            // TODO: make this more robust in case of missing data
+		let r = await fetch(url)
+			.then((e) => {
+				// handle HTTP response code
+				if (e.ok) {
+					return e.json();
+				}
+				// TODO: handle errors
+			})
+			.then((data) => {
+				// TODO: make this more robust in case of missing data
 
-            // NOTE: this will be a list of enrollment values
-            let temp = data.filter(itm => Number.parseInt(itm["sex"]) === 99);
-            temp = temp.map(itm => [Number.parseInt(itm["race"]), Number.parseInt(itm["enrollment_fall"])])
-            return Array(temp).sort();
-        });
+				// NOTE: this will be a list of enrollment values
+				let temp = data.filter(
+					(itm) => Number.parseInt(itm["sex"]) === 99
+				);
+				temp = temp.map((itm) => [
+					Number.parseInt(itm["race"]),
+					Number.parseInt(itm["enrollment_fall"]),
+				]);
+				return Array(temp).sort();
+			});
 
-        res.push(r);
-    }
+		res.push(r);
+	}
 
-    return unzip(res).map(unzip).map(p => { return { label: p[0][0], data: p[1]}; });
+	return unzip(res)
+		.map(unzip)
+		.map((p) => {
+			return { label: p[0][0], data: p[1] };
+		});
 }
 
 // get overall undergrad fall retention rates for years from start to end, inclusive
-async function getFallRetention(unitid, start=_.EARLIEST, end=_.LATEST) {
-    if (!unitid) { return null; }
+async function getFallRetention(unitid, start = _.EARLIEST, end = _.LATEST) {
+	if (!unitid) {
+		return null;
+	}
 
-    const api = API + "fall-retention"
+	const api = API + "fall-retention";
 
-    // supported in all browsers that matter, i.e. not IE
-    let params = new URLSearchParams();
-    params.append("unitid", unitid);
-    // code for total student body data
-    params.append("ftpt", 99);
+	// supported in all browsers that matter, i.e. not IE
+	let params = new URLSearchParams();
+	params.append("unitid", unitid);
+	// code for total student body data
+	params.append("ftpt", 99);
 
-    let res = []
-    for (let year=start; year<=end; year++) {
-        let url = new URL(`${api}/${year}/?${params}`, BASE);
+	let res = [];
+	for (let year = start; year <= end; year++) {
+		let url = new URL(`${api}/${year}/?${params}`, BASE);
 
-        let r = await fetch(url).then((e) => {
-            // handle HTTP response code
-            if (e.ok) {
-                return e.json();
-            }
-            // TODO: handle errors
-        }).then((data) => {
-            // TODO: make this more robust in case of missing data
-            return data.results[0]["retention_rate"];
-        });
+		let r = await fetch(url)
+			.then((e) => {
+				// handle HTTP response code
+				if (e.ok) {
+					return e.json();
+				}
+				// TODO: handle errors
+			})
+			.then((data) => {
+				// TODO: make this more robust in case of missing data
+				return data.results[0]["retention_rate"];
+			});
 
-        // TODO: package data in a better way
-        if (r) {
-            res.push(r);
-        }
-    }
+		// TODO: package data in a better way
+		if (r) {
+			res.push(r);
+		}
+	}
 
-    return res;
+	return res;
 }
--- src/apis/utils.js
+++ src/apis/utils.js
@@ -1,46 +1,45 @@
 export { mergeArrays, range, unzip, zip };
 
-
 // sorted merge, keeping exactly one copy of each unique element
 function mergeArrays(a, b) {
-    let i = 0;
-    let res = [];
-    res = res.concat(a).sort();
-    for (let n of b) {
-        i = res.findIndex(e => n <= e);
-        if (i === -1) {
-            // n is the max, so push it to the back
-            res.push(n);
-        } else if (n !== res[i]) {
-            // insert in sorted order
-            res.splice(i, 0, n);
-        }
-        // else if n is already in the array do nothing
-    }
-    return res;
+	let i = 0;
+	let res = [];
+	res = res.concat(a).sort();
+	for (let n of b) {
+		i = res.findIndex((e) => n <= e);
+		if (i === -1) {
+			// n is the max, so push it to the back
+			res.push(n);
+		} else if (n !== res[i]) {
+			// insert in sorted order
+			res.splice(i, 0, n);
+		}
+		// else if n is already in the array do nothing
+	}
+	return res;
 }
 
 // Range from start (inclusive) to end (exclusive)
-function range(start, end, interval=1) {
-    const diff = end - start;
-    const len = Math.floor(diff / interval + !!(diff % interval));
-    return [...Array(len).keys()].map(i => interval * i + start);
+function range(start, end, interval = 1) {
+	const diff = end - start;
+	const len = Math.floor(diff / interval + !!(diff % interval));
+	return [...Array(len).keys()].map((i) => interval * i + start);
 }
 
 function unzip(arr) {
-    const inner_len = Math.min(...arr.map(a => a.length));
+	const inner_len = Math.min(...arr.map((a) => a.length));
 
-    return range(0, inner_len).map(i => arr.map(a => a[i]));
+	return range(0, inner_len).map((i) => arr.map((a) => a[i]));
 }
 
 // zip :: [A] -> [B] -> [[A, B]]
 function zip(a, b) {
-    const len = Math.min(a.length, b.length);
+	const len = Math.min(a.length, b.length);
 
-    let res = [];
-    for (let i = 0; i < len; i++) {
-        res.push([a[i], b[i]]);
-    }
+	let res = [];
+	for (let i = 0; i < len; i++) {
+		res.push([a[i], b[i]]);
+	}
 
-    return res;
+	return res;
 }
